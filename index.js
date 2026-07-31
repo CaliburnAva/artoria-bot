7b58c9b272961349fcb005467821a1724c5e61d0
CaliburnAva <hoyolak@hotmail.com>
2026-07-31 12:04:14 -0300

    Connect first command

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
+const wife = require("./commands/wife");
+
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
 client.once("clientReady", () => {
   console.log(`👑 ${client.user.tag} despertou!`);
+});
+
+client.on("messageCreate", (message) => {
+
+  if (message.author.bot) return;
+
+  if (message.content === "!wife") {
+    wife.execute(message);
+  }
+
 });
 
 client.login(process.env.TOKEN);
