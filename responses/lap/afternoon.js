dcd54536c42155c9eac4eaa2729f2394cf8187c9
CaliburnAva <hoyolak@hotmail.com>
2026-08-01 19:48:38 -0300

    Create afternoon.js

---
+++ responses/lap/afternoon.js
@@ -0,0 +1,71 @@
+module.exports = [
+
+`The afternoon is calm.
+
+A book rests forgotten beside her.
+
+You quietly lay your head upon her lap.
+
+...
+
+Artoria gently adjusts your position...
+
+making certain you're comfortable.
+
+Only then...
+
+does she resume absentmindedly stroking your hair.
+
+...
+
+She cannot help smiling.
+
+Not even a little.`,
+
+`You quietly settle against her.
+
+...
+
+For several minutes...
+
+neither of you says a single word.
+
+The only sound is the occasional turning of a page.
+
+...
+
+Without looking away from her book...
+
+she gently squeezes your shoulder.
+
+Just to remind you...
+
+she's still there.`,
+
+`A gentle breeze slips through the open window.
+
+...
+
+You close your eyes.
+
+Resting comfortably upon her lap.
+
+...
+
+Artoria quietly watches you for a long moment.
+
+...
+
+— ...
+
+A faint smile appears.
+
+She softly brushes another loose strand of hair away from your face.
+
+...
+
+She hopes you stay there...
+
+for as long as you wish.`
+
+];
