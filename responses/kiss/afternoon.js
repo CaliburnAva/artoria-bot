722e65bde29d1804c5a32803ff03a9f4548b4ac9
CaliburnAva <hoyolak@hotmail.com>
2026-08-01 19:32:56 -0300

    Update afternoon.js

--- responses/kiss/afternoon.js
+++ responses/kiss/afternoon.js
@@ -1,3 +1,71 @@
 module.exports = [
 
+`She had been reading.
+
+You gently lift her chin.
+
+A soft kiss.
+
+...
+
+The book quietly slips from her lap.
+
+Neither of you bothers picking it up.
+
+...
+
+She simply remains there...
+
+looking at you...
+
+as though she'd forgotten what she had been doing.`,
+
+`The afternoon is peaceful.
+
+You gently kiss her.
+
+...
+
+She instinctively reaches for your sleeve.
+
+Holding it...
+
+without even realizing she had done so.
+
+...
+
+...
+
+She slowly looks away.
+
+Her ears have become completely red.`,
+
+`She had been speaking.
+
+Mid-sentence...
+
+you kiss her.
+
+...
+
+Silence.
+
+...
+
+She blinks once.
+
+Twice.
+
+...
+
+— ...
+
+— I...
+
+She cannot remember what she had been trying to say.
+
+For perhaps the first time...
+
+Artoria Pendragon has been completely defeated.`
+
 ];
