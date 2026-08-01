f55d337ddedff0b7ddfcba006956f5ca20b1b7d5
CaliburnAva <hoyolak@hotmail.com>
2026-07-31 23:53:02 -0300

    Update affection.js

--- affection.js
+++ affection.js
@@ -1,13 +1,29 @@
 const affection = {
 
-  level: "D",
-
-  get() {
-    return this.level;
+  levels: {
+    D: 0,
+    C: 25,
+    B: 50,
+    A: 75,
+    EX: 100
   },
 
-  set(level) {
-    this.level = level;
+  current: "D",
+
+  get() {
+    return this.current;
+  },
+
+  set(rank) {
+
+    if (this.levels[rank] !== undefined) {
+      this.current = rank;
+    }
+
+  },
+
+  value() {
+    return this.levels[this.current];
   }
 
 };
